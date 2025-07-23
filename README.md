92b52a9b69c79be06ad31455f18d7a1c9dc66bf7
Stromeagle2k <conman.ethan@gmail.com>
2025-07-23 16:39:01 -0400

    Created Background image, and set FPS limit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Readme
-
-Read me
+Features:
+Dash(a short dash witht he spacebar)
+Jumping(W key)
+Left-Right Movement(WAD keys)
+Side-scrolling
+Gravity
+Start and End Points
--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 import pygame
+from pygame.locals import *
+import sys
+ 
+pygame.init()
+window = pygame.display.set_mode((800, 800))
+pygame.display.set_caption(title='Square Hop')
+
+bground = pygame.image.load('PixelCaveArt2.png')
+
+FPS = pygame.time.Clock()
+FPS.tick(30)
+
+# This keeps the game open. Press backspace to close.
+run = True
+while run:
+    window.blit(bground, (0,0))
+    for e in pygame.event.get():
+        if e.type == QUIT or (e.type == KEYDOWN and e.key == K_BACKSPACE):
+            run = False
+
+    
+    pygame.display.update()
+
