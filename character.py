c2642f9f55e870dc35513ebaf0b51d17f491c6bd
Stromeagle2k <conman.ethan@gmail.com>
2025-07-24 13:17:44 -0400

    added player movement

--- character.py
+++ character.py
@@ -2,7 +2,39 @@ import pygame
 from pygame.locals import *
 
 class HopCube(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,x,y):
         super().__init__()
+        self.x = x
+        self.y = y
+        self.width = 25
+        self.height = 25
         self.surf = pygame.Surface((25,25))
         self.surf.fill((0,0,255))
+        self.rect = pygame.rect.Rect((self.x, self.y, self.width, self.height))
+        self.speed = [0,0]
+        self.momentum = 0
+
+       
+    def move(self, x):
+        self.x = x
+
+    def getspeedx(self):
+        return self.speed[0]
+    
+    def getspeedy(self):
+        return self.speed[1]
+
+    def update(self, *args, **kwargs):
+        self.rect.move_ip(1, 0)
+        return super().update(*args, **kwargs)
+            
+
+    def jump(self):
+        pass
+
+    def die(self):
+        pass
+
+    
+
+
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ FPS.tick(30)
 
 bground = Background('PixelCaveArt2.png', [0,0])
 
-player = HopCube()
+player = HopCube(0, 0)
 
 # This keeps the game open. Press backspace to close.
 run = True
@@ -23,10 +23,14 @@ while run:
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_BACKSPACE):
             run = False
 
+    player.move(500)
+    player.update()
+    print(player.rect.x)
+
     
     window.fill([255, 255, 255])
     window.blit(bground.image, bground.rect)
-    window.blit(player.surf, [0,0], )
+    window.blit(player.surf, player.rect.center)
     
     pygame.display.flip()
     pygame.display.update()
